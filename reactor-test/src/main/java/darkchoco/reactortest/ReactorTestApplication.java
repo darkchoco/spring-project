7698c4bed2f85876efd8d939215fa1e4aaf6f071
darkchoco <darkchoco@cloudscape.kr>
2023-01-27 13:33:03 +0100

    [reactor-test] 1st commit.

---
+++ reactor-test/src/main/java/darkchoco/reactortest/ReactorTestApplication.java
@@ -0,0 +1,12 @@
+package darkchoco.reactortest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactorTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactorTestApplication.class, args);
+    }
+}
---
+++ reactor-test/src/test/java/darkchoco/reactortest/ReactorTestApplicationTests.java
@@ -0,0 +1,12 @@
+package io.github.darkchoco.reactortest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactorTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
