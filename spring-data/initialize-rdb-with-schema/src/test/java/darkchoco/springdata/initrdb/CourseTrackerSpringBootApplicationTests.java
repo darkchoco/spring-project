0915a261b6611c4ab78539732c0f594fda4f46ac
darkchoco <darkchoco.ice@gmail.com>
2023-02-17 22:04:27 +0100

    [spring-data] initialize-rdb-with-schema project added.

---
+++ spring-data/initialize-rdb-with-schema/src/test/java/darkchoco/springdata/initrdb/CourseTrackerSpringBootApplicationTests.java
@@ -0,0 +1,42 @@
+package darkchoco.springdata.initrdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.sql.DataSource;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class CourseTrackerSpringBootApplicationTests {
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    public void givenDatasourceAvailableWhenAccessDetailsThenExpectDetails() throws SQLException {
+        assertThat(dataSource.getClass().getName()).isEqualTo("com.zaxxer.hikari.HikariDataSource");
+        assertThat(dataSource.getConnection().getMetaData().getDatabaseProductName()).isEqualTo("H2");
+    }
+
+    @Test
+    public void whenCountAllCoursesThenExpectFiveCourses() throws SQLException {
+        int noOfCourses = 0;
+        try (PreparedStatement ps = dataSource.getConnection().prepareStatement("SELECT COUNT(1) FROM COURSES");
+             ResultSet rs = ps.executeQuery()) {
+            while (rs.next()) {
+                noOfCourses = rs.getInt(1);
+
+            }
+            assertThat(noOfCourses).isEqualTo(5L);
+        }
+    }
+}
