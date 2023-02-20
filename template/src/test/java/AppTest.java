bd58b1c929dfb5c564528b67714662c9699c1c30
darkchoco <darkchoco@cloudscape.kr>
2023-02-20 11:32:15 +0100

    Dir reorg. and 'template' added.

---
+++ template/src/test/java/AppTest.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertThat(true).isTrue();
+    }
+}
