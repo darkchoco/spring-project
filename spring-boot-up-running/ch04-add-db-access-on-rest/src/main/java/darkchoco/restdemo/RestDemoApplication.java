b59740a085d5ff8dc6d0258d87b4ef26ed40659f
darkchoco <darkchoco@cloudscape.kr>
2023-03-24 15:16:52 +0100

    [sbur] ch04-add-db-access-on-rest added.

---
+++ spring-boot-up-running/ch04-add-db-access-on-rest/src/main/java/darkchoco/restdemo/RestDemoApplication.java
@@ -0,0 +1,98 @@
+package darkchoco.restdemo;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@SpringBootApplication
+public class RestDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestDemoApplication.class, args);
+	}
+}
+
+@RequiredArgsConstructor
+@Component
+class DataLoader {
+
+	private final CoffeeRepository coffeeRepository;
+
+	@PostConstruct
+	private void loadData() {
+		coffeeRepository.saveAll(List.of(
+				new Coffee("Café Cereza"),
+				new Coffee("Café Ganador"),
+				new Coffee("Café Lareño"),
+				new Coffee("Café Três Pontas")
+		));
+	}
+}
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/coffees")
+class RestApiDemoController {
+
+	private final CoffeeRepository coffeeRepository;
+
+	@GetMapping
+	Iterable<Coffee> getCoffees() {
+		return coffeeRepository.findAll();
+	}
+
+	@GetMapping("/{id}")
+	Optional<Coffee> getCoffeeById(@PathVariable String id) {
+		return coffeeRepository.findById(id);
+	}
+
+	@PostMapping
+	Coffee postCoffee(@RequestBody Coffee coffee) {
+		return coffeeRepository.save(coffee);
+	}
+
+	@PutMapping("/{id}")
+	ResponseEntity<Coffee> putCoffee(@PathVariable String id,
+									 @RequestBody Coffee coffee) {
+		return (coffeeRepository.existsById(id))
+				? new ResponseEntity<>(coffeeRepository.save(coffee), HttpStatus.OK)
+				: new ResponseEntity<>(coffeeRepository.save(coffee), HttpStatus.CREATED);
+	}
+
+	@DeleteMapping("/{id}")
+	void deleteCoffee(@PathVariable String id) {
+		coffeeRepository.deleteById(id);
+	}
+}
+
+interface CoffeeRepository extends CrudRepository<Coffee, String> {}
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Entity
+class Coffee {
+
+    @Id
+    private String id;
+    private String name;
+
+    public Coffee(String name) {
+        this(UUID.randomUUID().toString(), name);
+    }
+}
