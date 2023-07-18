6cc3bd5c346c21361405e61bd0c8ab3e44a1caed
darkchoco <darkchoco@cloudscape.kr>
2023-07-18 13:47:06 +0200

    [sbur] ch06-sbur-repository added. (using repository instead of template)

---
+++ spring-boot-up-running/ch06/ch06-sbur-repository/src/main/java/darkchoco/data/PlaneFinderPoller.java
@@ -0,0 +1,33 @@
+package darkchoco.data;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@RequiredArgsConstructor
+@EnableScheduling
+@Component
+class PlaneFinderPoller {
+    private final WebClient client =
+            WebClient.create("http://localhost:7634/aircraft");
+
+    private final RedisConnectionFactory connectionFactory;
+    private final AircraftRepository repository;
+
+    @Scheduled(fixedRate = 1000)
+    private void pollPlanes() {
+        connectionFactory.getConnection().serverCommands().flushDb();
+
+        client.get()
+                .retrieve()
+                .bodyToFlux(Aircraft.class)  // Flux로 변환
+                .filter(plane -> !plane.getReg().isEmpty())  // 등록번호 없는 Aircraft 제외
+                .toStream()
+                .forEach(repository::save);
+
+        repository.findAll().forEach(System.out::println);
+    }
+}
