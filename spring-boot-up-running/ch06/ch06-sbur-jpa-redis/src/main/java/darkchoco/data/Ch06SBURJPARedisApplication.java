1513d34a8f6609c1a44606f08ad52a9cf59ecd1a
darkchoco <darkchoco@cloudscape.kr>
2023-11-30 13:33:43 +0100

    [sbur] ch06-sbur-jpa-redis added. (JPA ver. instead of Template)

---
+++ spring-boot-up-running/ch06/ch06-sbur-jpa-redis/src/main/java/darkchoco/data/Ch06SBURJPARedisApplication.java
@@ -0,0 +1,12 @@
+package darkchoco.data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch06SBURJPARedisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ch06SBURJPARedisApplication.class, args);
+	}
+}
