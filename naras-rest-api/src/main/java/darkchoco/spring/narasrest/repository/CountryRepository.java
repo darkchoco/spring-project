613641a67da98af712832ce88bdefba65fa247b2
darkchoco <darkchoco.ice@gmail.com>
2024-11-09 17:55:28 +0100

    [nra] naras-rest-api, N+1 problem solved.

--- naras-rest-api/src/main/java/darkchoco/spring/narasrest/repository/CountryRepository.java
+++ naras-rest-api/src/main/java/darkchoco/spring/narasrest/repository/CountryRepository.java
@@ -2,20 +2,21 @@ package darkchoco.spring.narasrest.repository;
 
 import darkchoco.spring.narasrest.domain.Country;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+//import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.EntityGraph;
 
 import java.util.List;
 
 public interface CountryRepository extends JpaRepository<Country, String> {
     List<Country> findByCommonNameContainingIgnoreCase(String commonName);
 
-//    @Query("""
-//            SELECT cc.capital, co.code, co.commonName, co.officialName, co.flagEmoji, co.flagImg, co.region, co.population
-//            FROM Country co
-//            INNER JOIN CountryCapital cc ON co.code = cc.country_code""")
-    @Query(value = """
-            SELECT cc.capital, co.code, co.common_name, co.official_name, co.flag_emoji, co.flag_img, co.region, co.population
-            FROM country co
-            INNER JOIN country_capital cc ON co.code = cc.code""", nativeQuery = true)
-    List<Country> findAllWithJoin();
+//    @Query(value = """
+//            SELECT cc.capital, co.code, co.common_name, co.official_name, co.flag_emoji, co.flag_img, co.region, co.population
+//            FROM country co
+//            INNER JOIN country_capital cc ON co.code = cc.code""", nativeQuery = true)
+//    List<Country> findAllWithJoin();
+
+    @SuppressWarnings("NullableProblems")
+    @EntityGraph(attributePaths = {"capital"})
+    List<Country> findAll();
 }
