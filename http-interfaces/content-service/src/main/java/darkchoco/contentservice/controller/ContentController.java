b544ab051b41c95a4334550fbcdc0f1c59d9e8d2
darkchoco <darkchoco.ice@gmail.com>
2025-01-26 17:37:55 +0100

    http-interfaces pjt completed.

---
+++ http-interfaces/content-service/src/main/java/darkchoco/contentservice/controller/ContentController.java
@@ -0,0 +1,45 @@
+package darkchoco.contentservice.controller;
+
+import darkchoco.contentservice.model.Article;
+import darkchoco.contentservice.service.ArticleClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/content")
+public class ContentController {
+
+    public final ArticleClient articleClient;
+
+    public ContentController(ArticleClient articleClient) {
+        this.articleClient = articleClient;
+    }
+
+    @GetMapping("/articles")
+    public ResponseEntity<List<Article>> findAllArticles() {
+        return articleClient.findAll();
+    }
+
+    @GetMapping("/articles/{id}")
+    public Optional<Article> findById(@PathVariable Integer id) {
+        return articleClient.findOne(id);
+    }
+
+    @PostMapping("/articles")
+    public void create(@RequestBody Article article) {
+        articleClient.create(article);
+    }
+
+    @PutMapping("/articles/{id}")
+    public void update(@RequestBody Article article, @PathVariable Integer id) {
+        articleClient.update(article,id);
+    }
+
+    @DeleteMapping("/articles/{id}")
+    public void delete(@PathVariable Integer id) {
+        articleClient.delete(id);
+    }
+}
---
+++ http-interfaces/content-service/src/main/java/darkchoco/contentservice/model/Article.java
@@ -0,0 +1,4 @@
+package darkchoco.contentservice.model;
+
+public record Article(Integer id, String title, String body) {
+}
