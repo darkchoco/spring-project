e4b7a47d6f3184b4df38c5cfa831a5b69a2b1fdc
darkchoco <darkchoco.ice@gmail.com>
2025-02-11 21:27:03 +0100

    [ssia2] ssia-ch2-ex3 updated.

--- spring-security/ssia2/ssia-ch2-ex3/src/main/java/darkchoco/ssia/config/ProjectConfig.java
+++ spring-security/ssia2/ssia-ch2-ex3/src/main/java/darkchoco/ssia/config/ProjectConfig.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
@@ -23,17 +22,19 @@ public class ProjectConfig {
                 c -> c.anyRequest().permitAll()
         );
 
-        return http.build();
-    }
-
-    @Bean
-    UserDetailsService userDetailsService() {
+        // 1. 새로운 유저 생성
         var user = User.withUsername("john")
                 .password("12345")
                 .authorities("read")  // 아무 문자열이나 일단 지정
                 .build();
 
-        return new InMemoryUserDetailsManager(user);
+        // 2. UserDetailsService에서 관리하도록 유저 추가
+        var userDetailsService = new InMemoryUserDetailsManager(user);
+
+        // 3. UserDetailsService 구성
+        http.userDetailsService(userDetailsService);
+
+        return http.build();
     }
 
     @Bean
