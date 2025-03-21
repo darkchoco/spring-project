6452ccba0d22a9ea3b9c76be1fcb0a36b72990d8
darkchoco <darkchoco.ice@gmail.com>
2025-03-21 15:53:20 +0100

    [ss02m] ss02m-03 updated with PasswordEncoder.

--- spring-security/ss02m/ss02m-03/src/main/java/darkchoco/ss02m/config/SecurityConfiguration.java
+++ spring-security/ss02m/ss02m-03/src/main/java/darkchoco/ss02m/config/SecurityConfiguration.java
@@ -7,6 +7,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -29,7 +31,15 @@ public class SecurityConfiguration {
         // You have entered a password with no PasswordEncoder. If that is your intent, it should be prefixed with `{noop}`.
         // 즉 아래 패스워드에 {noop}을 적어놓지 않으면 에러 발생.
         UserDetails user = User.withUsername("user").password("{noop}12345").authorities("read").build();
-        UserDetails admin = User.withUsername("admin").password("{noop}54321").authorities("admin").build();
+        UserDetails admin = User.withUsername("admin")
+                .password("{bcrypt}$2a$12$uvno3BTHwGOmxqw25IQZhOipZQi83/V5gICuHblog1EC5xhUnI2YG")  // bcrypt 가 현재는 default라서 이렇게 안 적어주어도 되지만 readibility를 위해.
+                .authorities("admin")
+                .build();
         return new InMemoryUserDetailsManager(user, admin);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 }
